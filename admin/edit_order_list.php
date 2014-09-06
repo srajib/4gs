7f508f90ad845e8ddfa3c49a2d201c2e66b320cc
srajib <rajib1111@gmail.com>
2014-09-06 14:08:01 +0600

    order status change by admin

--- admin/edit_order_list.php
+++ admin/edit_order_list.php
@@ -1,4 +1,4 @@
-<?php error_reporting(E_ERROR);	session_start();
+<?php error_reporting(E_ERROR);	session_start(); include('lib/connect.php');
 if(!isset($_SESSION['id']))
 {
 	echo "<meta http-equiv='refresh' content='0; url=index.php'>";
@@ -37,10 +37,6 @@ if($_SESSION['authority']==6)
 <script src="./js/Slate.js"></script>
 <script src="./js/demos/demo.validate.js"></script>
 
-<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
 
 </head>
 
@@ -87,85 +83,75 @@ if($_SESSION['authority']==6)
 <?php include('menu.php'); ?>
 
 
-
 <div id="content">
 		
-	<div class="container">
-		
-		<div id="page-title" class="clearfix">
-			
-			<ul class="breadcrumb">
-			  <li>
-			    <a href="./">Home</a> <span class="divider">/</span>
-			  </li>
-			  <li class="active">Menu</li>
-			</ul>
-			
-		</div> <!-- /.page-title -->
-<?php include('lib/connect.php');
+<div class="container">
 
-
+<?php 
 if(isset($_POST['submit']))
 {
     $id=$_POST['id'];	
 	$result=mysql_query("UPDATE order_list SET ord_status='$_POST[ordstatus]' WHERE ord_id=$id");
-
+	
 	if($result)
 	{
-		$con = "<b><font face='Arial, Helvetica, sans-serif' size='2' color='#006600'>Order saved successfully.</font></b>";
+		$con = "<b><font face='Arial, Helvetica, sans-serif' size='2' color='#060'>Order has updated successfully.</font></b>";
+		// mail($to,$subject,$message,$headers);
 	}
 	else
 	{
-		$con = "<b><font face='Arial, Helvetica, sans-serif' size='2' color='#FF0000'>Order could not be saved.</font></b>";
+		$con = "<b><font face='Arial, Helvetica, sans-serif' size='2' color='#F00'>Order could not be updated!</font></b>";
 	}
 }
 else
 {
 	$con = "";
-    $id=$_GET['id'];	
+    $id = $_GET['id'];	
 }
 ?> 			
-		<div class="row">
-			
-			<div class="span8">
-	      		
-	      		<div id="horizontal" class="widget widget-form">
-	      			
-	      			<div class="widget-header">	      				
-	      				<h3>
-	      					<i class="icon-pencil"></i>
-	      					Edit Order
-      					</h3>	
-      				</div> <!-- /widget-header -->
-					
-					<div class="widget-content">
-					<?php echo $con; ?>						
-					
-						<form class="form-horizontal" action="edit_order_list.php" method="post">
-					        <fieldset>
-					         
-								<?php
-								$sqlcat = mysql_query("SELECT * FROM order_list WHERE ord_id=$id");
-								while($row2 = mysql_fetch_array($sqlcat))
-								{
-								?> 
-								
-																	
-							 <div class="control-group">
-					            <label class="control-label" for="input01">Order ID</label>
-					            <div class="controls">
-					             <?php echo $row2['ord_id']; 
-									echo "<input type=hidden name=id value=".$row2['ord_id'].">";					          
-								?>  								
-					            </div>
-					          </div>						  
+
+<div class="row">
+
+<div class="span8">
+
+<div id="horizontal" class="widget widget-form">
+
+<div class="widget-header">	      				
+<h3>
+<i class="icon-pencil"></i>
+Edit Order
+</h3>	
+</div> <!-- /widget-header -->
+
+<div class="widget-content">
+<?php echo $con; ?>						
+
+<form class="form-horizontal" action="edit_order_list-2.php" method="post">
+
+<fieldset>
+
+<?php $sqlcat = mysql_query("SELECT * FROM order_list WHERE ord_id=$id");
+while($row2 = mysql_fetch_array($sqlcat))
+{
+?> 
+
+<div class="control-group">
+<label class="control-label" for="input01">Order ID</label>
+<div class="controls">
+<?php 
+	echo $row2['ord_id']; 
+	echo "<input type=hidden name=id value=".$row2['ord_id'].">";					          
+?>  								
+</div>
+</div>						  
+
 							  
 <?php $x=$row2['ord_status']; ?>
 
 <div class="control-group">
 <label class="control-label" for="input01">Order Status</label>
 <div class="controls">								
-	<select name="ordstatus" id="input01" value="<?php echo $row2['ord_status']; ?>"> 
+	<select name="ordstatus" id="input01" value="<?php echo $x; // echo $row2['ord_status']; ?>"> 
 		<option value="0" <?php if($x=='0'){ ?> selected="selected" <?php } ?> >Pending</option>
 		<option value="1" <?php if($x=='1'){ ?> selected="selected" <?php } ?> >Excepted</option>
 		<option value="2" <?php if($x=='2'){ ?> selected="selected" <?php } ?> >Wait</option>		
@@ -173,26 +159,26 @@ else
 		<option value="4" <?php if($x=='4'){ ?> selected="selected" <?php } ?> >Collect</option>		
 		<option value="5" <?php if($x=='5'){ ?> selected="selected" <?php } ?> >Return</option>		
 		<option value="6" <?php if($x=='6'){ ?> selected="selected" <?php } ?> >Refund</option>		
-		<option value="7" <?php if($x=='7'){ ?> selected="selected" <?php } ?> >Successfuly Received</option>		
-		<option value="8" <?php if($x=='8'){ ?> selected="selected" <?php } ?> >Successfuly Stocked</option>		
+		<option value="7" <?php if($x=='7'){ ?> selected="selected" <?php } ?> >Successfully Received</option>		
+		<option value="8" <?php if($x=='8'){ ?> selected="selected" <?php } ?> >Successfully Stocked</option>		
 		<option value="9" <?php if($x=='9'){ ?> selected="selected" <?php } ?> >Return to Origin</option>			
 	</select>
 </div>
 </div>
                               
-					          <?php
-								}
-							  ?> 
-					          <div class="form-actions">
-					            <button type="submit" name="submit" class="btn btn-primary btn-large">Edit Order</button>
-					            <!-- <button class="btn btn-large">Delete</button> -->
-					          </div>
-					        </fieldset>
-					      </form>	
-						
-					</div> <!-- /widget-content -->						
-				</div>	
-                </div>	    		
+<?php
+}
+?> 
+<div class="form-actions">
+<button type="submit" name="submit" class="btn btn-primary btn-large">Edit Order</button>
+<!-- <button class="btn btn-large">Delete</button> -->
+</div>
+</fieldset>
+</form>	
+
+</div> <!-- /widget-content -->						
+</div>
+</div>
 		
 </div> <!-- /.container -->	
 </div> <!-- /#content -->
